69c0c46146ed7c166e2c05952d264433462a186d
byron <47872200+byeoon@users.noreply.github.com>
2024-04-03 15:39:44 +0000

    initial push - broken stuff tho

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,27 @@
 import { Plugin, registerPlugin } from 'enmity/managers/plugins';
-import { React } from 'enmity/metro/common';
+import { React, Toasts } from 'enmity/metro/common';
 import { getByProps } from 'enmity/metro';
 import { create } from 'enmity/patcher';
+import { Text, ScrollView, TouchableOpacity, FormRow, View } from "enmity/components";
 import manifest from '../manifest.json';
+import findInReactTree from 'enmity/utilities/findInReactTree';
+import {getIDByName} from "enmity/api/assets";
 
-import Settings from './components/Settings';
-
-const Typing = getByProps('startTyping');
-const Patcher = create('silent-typing');
-
-const SilentTyping: Plugin = {
+const LazyActionSheet = getByProps("openLazy", "hideActionSheet");
+const Patcher = create('ReadEverything');
+const ReadEverything: Plugin = {
    ...manifest,
 
    onStart() {
-      Patcher.instead(Typing, 'startTyping', () => { });
-      Patcher.instead(Typing, 'stopTyping', () => { });
+      Patcher.after(Component, 'default', (_, args, res) => {
+         res.props.children ??= [];
+         // "Push our child (LOL)."
+         res.props.children.push(<View>Test</View>);
+      });
    },
 
    onStop() {
       Patcher.unpatchAll();
-   },
-
-   getSettingsPanel({ settings }) {
-      return <Settings settings={settings} />;
    }
 };
-
-registerPlugin(SilentTyping);
+registerPlugin(ReadEverything);
